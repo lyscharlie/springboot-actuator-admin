64c4ebd4201c6ad2d4f5c51a327afc61a86623ce
lycharlie <lyscharlie@hotmail.com>
2020-03-11 09:04:24 +0800

    11111

---
+++ src/main/java/com/lyscharlie/biz/service/UserService.java
@@ -0,0 +1,8 @@
+package com.lyscharlie.biz.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lyscharlie.biz.entity.UserDO;
+
+public interface UserService extends IService<UserDO> {
+
+}
