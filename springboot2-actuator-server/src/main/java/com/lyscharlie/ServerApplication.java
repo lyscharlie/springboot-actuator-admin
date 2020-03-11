f678c64debf0a0d5273b70131ed92e67bafd6b1c
lycharlie <lyscharlie@hotmail.com>
2020-03-11 16:46:44 +0800

    1111

---
+++ springboot2-actuator-server/src/main/java/com/lyscharlie/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.lyscharlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
