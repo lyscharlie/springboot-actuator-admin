7e3b8876118ff978bd3e99040cb83b74a04d0f2a
liyishi <lyscharlie@hotmail.com>
2022-06-27 13:51:32 +0800

    111

--- springboot2-actuator-server/src/main/java/com/lyscharlie/common/SecurityConfig.java
+++ springboot2-actuator-server/src/main/java/com/lyscharlie/common/SecurityConfig.java
@@ -1,26 +1,23 @@
 package com.lyscharlie.common;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
-
 @Configuration
-public class SecuritySecureConfig {
+@RequiredArgsConstructor
+public class SecurityConfig {
 
-	private final String adminContextPath;
-
-	public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
-		this.adminContextPath = adminServerProperties.getContextPath();
-	}
+	private final AdminServerProperties adminServerProperties;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		String adminContextPath =  adminServerProperties.getContextPath();
 		SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
 		successHandler.setTargetUrlParameter("redirectTo");
 		successHandler.setDefaultTargetUrl(adminContextPath + "/");
